310e4cf7b43abb8572f53b0214e45fb6371eb425
Konstantin <tuskancik228@gmail.com>
2021-10-27 16:32:51 +0300

    finish page 404

--- src/views/appPage404.vue
+++ src/views/appPage404.vue
@@ -1,5 +1,18 @@
 <template>
-    <header class="header">404</header>
+    <header class="header flex-column">
+        <h1>Ошибка 404</h1>
+    </header>
+    <main class="main flex-column card-shadow">
+        <section class="flex-row error">
+            <img class="error-img" src="../assets/tower_of_pisa.gif" alt="tower">
+            <div class="error-text">
+                <h3>Ой! Башня Упала!</h3>
+                <h3>Кажется, Вы ввели не верный адрес, или страница была удалена.</h3>
+                <h3>Попробуйте еще раз, с Главной страницы.</h3>
+            </div>
+        </section>
+        <button class="btn">Перейти на главную</button>
+    </main>
 </template>
 
 <script>
@@ -9,5 +22,43 @@
 </script>
 
 <style scoped>
-
+    .main {
+        justify-content: space-evenly;
+        align-items: center;
+        margin: 0 2rem;
+        padding: 2rem;
+        background-color: #fdfdfd;
+    }
+        .main .btn {
+            margin: 2rem 0 0 0;
+            width: 90%;
+        }
+    .error {
+        width: 100%;
+    }
+    .error-img {
+        width: 100%;
+        max-width: 25rem;
+        max-height: 25rem;
+    }
+    .error-text {
+        flex-grow: 1;
+        max-width: 40rem;
+    }
+    @media (max-width: 650px) {
+        .header {
+            margin-bottom: 0;
+        }
+        .main {
+            justify-content: space-evenly;
+            align-items: center;
+            margin: 0 1rem;
+            padding: 1.5rem;
+            background-color: #fdfdfd;
+        }
+            .main .btn {
+                margin: 0;
+                width: 100%;
+            }
+    }
 </style>
