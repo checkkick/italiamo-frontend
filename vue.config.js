dfaba0e98be1ad48677e9c7f3271b1b8c6c61f0f
checkkick-home <kost.anisimov.2012@yandex.ru>
2022-01-11 15:52:50 +0300

    add prerender-spa

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,32 @@
+var path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const PuppeteerRenderer = PrerenderSPAPlugin.PuppeteerRenderer
+
 module.exports = {
-    transpileDependencies: ['vue-meta']
+	configureWebpack: {
+		plugins: [
+			new PrerenderSPAPlugin({
+				staticDir: path.join(__dirname, 'dist'), // The path to the folder where index.html is.
+				routes: [
+					'/',
+					'/news',
+					'/programs',
+					'/programs/:courseId',
+					'/prices',
+					'/teachers',
+					'/teachers/:teacherId',
+					'/comments',
+					'/oferta',
+					'/payment',
+					'/404',
+				], // List of routes to prerender.
+				renderer: new PuppeteerRenderer(),
+			}),
+		],
+	},
+	transpileDependencies: ['vue-meta'],
 }
+
+// module.exports = {
+// 	transpileDependencies: ['vue-meta'],
+// }
