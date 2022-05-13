a8c55065a18c8b3e23432a341e84d28ab8db64f5
checkkick work <tuskancik226@gmail.com>
2022-05-13 08:57:29 +0300

    delete block preices for exams

--- src/views/appPagePrices.vue
+++ src/views/appPagePrices.vue
@@ -50,38 +50,6 @@
 				<li>задания для закрепления материалов каждого занятия</li>
 			</ul>
 		</div>
-		<section class="flex-column">
-			<h2>Цены на подготовку к экзаменам</h2>
-			<h3>Подготовка к экзаменам происходит в мини-группе из 4-х человек</h3>
-			<div class="flex-row">
-				<div class="price-card">
-					<div class="price-card-title">
-						<h3>Международный экзамен А2 (CELI, CILS, PLIDA)</h3>
-						<h3>14 700 руб.</h3>
-					</div>
-					<div class="price-card-text">
-						<h4>Курс 16 часов</h4>
-						<p>занятия в мини-группе с преподавателем (один урок - 60 мин)</p>
-						<p>доступ в виртуальный класс с учебными материалами</p>
-						<p>задания для закрепления материалов каждого занятия</p>
-						<button @click="routerPush('/payment')" class="btn">Подробнее</button>
-					</div>
-				</div>
-				<div class="price-card">
-					<div class="price-card-title">
-						<h3>Международный экзамен В1, В2, С1 (CELI, CILS, PLIDA)</h3>
-						<h3>19 700 руб.</h3>
-					</div>
-					<div class="price-card-text">
-						<h4>Курс 24 часа</h4>
-						<p>занятия в мини-группе с преподавателем (один урок - 60 мин)</p>
-						<p>доступ в виртуальный класс с учебными материалами</p>
-						<p>задания для закрепления материалов каждого занятия</p>
-						<button @click="routerPush('/payment')" class="btn">Подробнее</button>
-					</div>
-				</div>
-			</div>
-		</section>
 	</main>
 </template>
 
@@ -135,31 +103,6 @@ export default {
 <style scoped>
 .flex-column {
 	align-items: center;
-}
-.price-card {
-	max-width: 35rem;
-	margin: 1rem;
-	border-radius: 15px;
-	background-color: rgba(255, 255, 255, 0.8);
-	box-shadow: 1px 10px 20px 0 rgba(0, 0, 0, 0.1);
-}
-.price-card-title {
-	padding: 1rem 2rem;
-	border-radius: 15px;
-	background-color: rgba(233, 187, 145, 0.75);
-}
-.price-card-text {
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	padding: 0 2rem;
-}
-.price-card-text p {
-	padding-bottom: 1rem;
-	border-bottom: 2px solid rgba(128, 128, 128, 0.4);
-}
-.price-card-text .btn {
-	align-self: stretch;
 }
 @media (max-width: 650px) {
 	h1 {
